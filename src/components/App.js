ae3579d21b4cc75797ab08aa44d703343c04b18e
ConniZMac <constanza.macnamara@gmail.com>
2022-08-04 17:38:24 -0400

    O.H

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,50 @@
+import pixar from "../data/pixar/pixar.js";
+const imageback= pixar.ball[0].image;
+console.log(imageback)
+let botonJuego = document.querySelector(".instrucciones")
+botonJuego.addEventListener("click", () => {
+    let audioUp = document.createElement("audio")
+    audioUp.setAttribute("src","sonido.mp3")
+    audioUp.play();
+})
+//console.log(pixar);
+let nombre = window.prompt("Ingresa tu nombre");
 
-import pixar from '../data/pixar/pixar.js';
-console.log(pixar);
-let nombre = window.prompt ("Ingresa tu nombre");
-
-alert("Bienvenid@, " + nombre + " a jugar Memory-Match Pixar!" );
+alert("Bienvenida, " + nombre + " a jugar Memory-Match Pixar!");
 
 const App = () => {
   
-  const container = document.createElement('div');
-  container.className='container';
-  const arrCards= generadorImagenes(pixar.items);
-  arrCards.className='arrcards';
-  for (let i = 0; i<arrCards.length; i++){
-    container.appendChild(arrCards[i]);
+  const container = document.createElement("div");//creamos el container de las imagenes
+  container.className = "container";// cremos la clase container, para modificarlo en css
+  const arrCards = generadorImagenes(pixar.items);
+  arrCards.className = "arrcards";// Contiene las imagenes
+  for (let i = 0; i < arrCards.length; i++) {
+    container.appendChild(arrCards[i]); //recorremos la data(Array)llama las imagenes al container
   }
 
   return container;
 };
 
-
-
-
-
-
-
-
-//const aleatorio = () => {
-  
-//};
-
-
-
 const generadorImagenes = (data) => {
-  data.sort(() => Math.random() - 0.5);
-  
+  data.sort(() => Math.random() - 0.5);// Se crea la función que mezcla las cartas.
+
   const arrCards = [];
-  data.forEach(item => {
+  data.forEach((item) => { //llama cada item de la Data.
     const carta = document.createElement("div");
-    carta.className='cartas'
+    carta.className = "cartas"; //creamos la variable carta ...creemos que es para darle funcionaladid a la carta , darla vuelta etc...
 
     const front = document.createElement("div");
-    front.className='front'
+    front.className = "front";
     const imgFront = document.createElement("img");
-    imgFront.className='frontimg'
-    imgFront.setAttribute('src', item.image);
+    imgFront.className = "frontimg";
+    imgFront.setAttribute("src", imageback);
+    imgFront.addEventListener("click",function(event){event.target.setAttribute("src",item.image)})
+
     front.appendChild(imgFront);
 
-    const back = document.createElement("div");
-
+    const back = document.createElement("img");
+    back.className="backimg";
+    
     carta.classList = "carta";
     front.classList = "front";
     back.classList = "back";
@@ -60,11 +57,5 @@ const generadorImagenes = (data) => {
 
   return arrCards;
 };
-
-
-
-
-
-
 
 export default App;
